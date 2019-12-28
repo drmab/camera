c6dd355e5e7c15ddd939d3e22ce3ebed0e67d33f
Marc Andre Belanger <belanger@langue.qc.ca>
2019-12-28 10:57:02 -0500

    Update camera2.py

--- camera2.py
+++ camera2.py
@@ -1,26 +1,29 @@
-from gpiozero import Button
+from gpiozero import LED, Button
 from picamera import PiCamera
 from datetime import datetime
 from signal import pause
 
-cameraButton = Button(3)
-videoButton = Button(4)
+cameraButton = Button(21)
+videoButton = Button(20)
+led = LED(12)
 camera = PiCamera()
 camera.start_preview()
 
 def capture():
+    led.on()
     timestamp = datetime.now().isoformat()
     camera.capture('/home/pi/%s.jpg' % timestamp)
+    led.off()
 
 def record():
+    led.on()
     timestamp = datetime.now().isoformat()
     camera.start_recording('/home/pi/%s.h264' % timestamp)
-
-def stopVideo():
+    videoButton.wait_for_release()
     camera.stop_recording()
+    led.off()
 
 cameraButton.when_pressed = capture
 videoButton.when_pressed = record
-videoButton.when_released = stopVideo
 
 pause()
