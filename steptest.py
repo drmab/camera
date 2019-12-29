fd10a5b3c08adbb928ed34c771d1e503147196ab
Marc Andre Belanger <belanger@langue.qc.ca>
2019-12-29 11:43:02 -0500

    gpis numbers

--- steptest.py
+++ steptest.py
@@ -1,31 +1,19 @@
 from gpiozero import OutputDevice
 from signal import pause
 
-Button.was_held = False
-
 btn = Button(21)
 
 # motor
-IN1 = OutputDevice(31)
-IN2 = OutputDevice(33)
-IN3 = OutputDevice(35)
-IN4 = OutputDevice(37)
+IN1 = OutputDevice(6)
+IN2 = OutputDevice(13)
+IN3 = OutputDevice(19)
+IN4 = OutputDevice(26)
 stepPins = [IN1,IN2,IN3,IN4]
 seq = [[1,0,0,0],
        [0,1,0,0],
        [0,0,1,0],
        [0,0,0,1]]
 stepCounter = 0
-
-
-def held(btn):
-    btn.was_held = True
-
-def released(btn):
-    if not btn.was_held:
-        stepOne()
-    btn.was_held = False
-
 
 def stepOne():
   for pin in range(0,4):
@@ -38,8 +26,7 @@ def stepOne():
   if (stepCounter >= 4):
     stepCounter = 0
 
-btn.when_held = held
-btn.when_released = released
+btn.when_pressed = stepOne
 
 pause()
 
