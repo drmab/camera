c43ee0e5ca1ab2f4f18a9c6e91c8863dd50b47b4
Marc Andre Belanger <belanger@langue.qc.ca>
2019-12-29 13:04:27 -0500

    Added stepper

--- camera2.py
+++ camera2.py
@@ -1,7 +1,24 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, OutputDevice
 from picamera import PiCamera
 from datetime import datetime
 from signal import pause
+from time import sleep
+
+# Motor
+IN1 = OutputDevice(6)
+IN2 = OutputDevice(13)
+IN3 = OutputDevice(19)
+IN4 = OutputDevice(26)
+stepPins = [IN1,IN2,IN3,IN4] # Motor GPIO pins</p><p>
+stepDir = 1
+
+seq = [[1,0,0,0], # Define step sequence as shown in manufacturers datasheet
+             [0,1,0,0],
+             [0,0,1,0],
+             [0,0,0,1]]
+#stepCount = 4
+waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
+#stepCounter = 0
 
 Button.was_held = False
 
@@ -10,16 +27,26 @@ led = LED(12)
 camera = PiCamera()
 camera.start_preview()
 
+def stepone():
+  for stepCounter in range(0,3):
+    for pin in range(0,4):
+      xPin=stepPins[pin]          # Get GPIO
+      if seq[stepCounter][pin]!=0:
+        xPin.on()
+      else:
+        xPin.off()
+    sleep(0.004) 
+
 def capture():
     led.on()
     timestamp = datetime.now().isoformat()
-    camera.capture('/home/pi/%s.jpg' % timestamp)
+    camera.capture('/home/pi/pictures/%s.jpg' % timestamp)
     led.off()
 
 def record():
     led.on()
     timestamp = datetime.now().isoformat()
-    camera.start_recording('/home/pi/%s.h264' % timestamp)
+    camera.start_recording('/home/pi/videos/%s.h264' % timestamp)
     videoButton.wait_for_release()
     camera.stop_recording()
     led.off()
@@ -37,4 +64,3 @@ btn.when_held = held
 btn.when_released = released
 
 pause()
-
